0cb385aa552c46363a8917e28e8abb408764bdac
Christopher <cjsing11@gmail.com>
2018-08-02 15:40:49 -0400

    added properties and constructor for 'Product' class.

--- PointOfServiceMidterm/Product.cs
+++ PointOfServiceMidterm/Product.cs
@@ -8,5 +8,39 @@ namespace PointOfServiceMidterm
 {
     class Product
     {
+
+        private string name;
+        private string description;
+        private double price;
+        private string category;
+
+        public string Name { get; set; }
+
+        private string Category { get; set; }
+
+        public string Description { get; set; }
+
+        private double Price { get; set; }
+        
+        //Constructors
+
+        public Product()// default constructor (no argument constructor)
+        {
+            Name = "not assigned";
+            Category = "not assigned";
+            Description = "not assigned";
+            Price = 0;
+        }
+
+        public Product(string nam, string cat, string desc, double pr)
+        {
+            Name = nam;
+            Category = cat;
+            Description = desc;
+            Price = pr;
+        }
+
+
+
     }
 }
