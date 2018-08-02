0f60b16394902df5245ed95ae815f1444337b012
chris <tarancello@gmail.com>
2018-08-02 15:35:07 -0400

    adding first validator

--- PointOfServiceMidterm/Product.cs
+++ PointOfServiceMidterm/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PointOfServiceMidterm
 {
-    class Coffee : Drink
+    class Product
     {
     }
 }
--- PointOfServiceMidterm/Validator.cs
+++ PointOfServiceMidterm/Validator.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PointOfServiceMidterm
 {
     class Validator
     {
+
+        public static int ListChoiceValidator(string askUser, string errorMessage, string rangeErrorMessage, int listCount)
+        {
+            Console.WriteLine(askUser);
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput == null)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else if (!Regex.IsMatch(userInput, @"^\d{1,9}$"))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else if (int.Parse(userInput) < 1 || int.Parse(userInput) > listCount)
+                {
+                    Console.WriteLine(rangeErrorMessage);
+                }
+                else
+                {
+                    return int.Parse(userInput);
+                }
+            }
+        }
+
     }
 }
