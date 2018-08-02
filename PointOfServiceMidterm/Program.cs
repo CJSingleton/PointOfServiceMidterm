29353bb3f9596d1496882a7e547ebd2480c470f4
kristen <thehostboy@gmail.com>
2018-08-02 16:39:13 -0400

    added Menu list to program.cs

--- PointOfServiceMidterm/Program.cs
+++ PointOfServiceMidterm/Program.cs
@@ -11,6 +11,18 @@ namespace PointOfServiceMidterm
     {
         static void Main(string[] args)
         {
+            //Product prod1 = new Product("1) Tomato & Mozzarella Sandwich", "Food:", "Baked tomatoes with melted mozzarella on toasted Halal bread.", 11.50);
+            //Product prod2 = new Product("2) Chicken BLT Sandwich", "Food:", "Grilled chicken with lettuce, tomato, and honey maple bacon on toasted rye bread.", 12.95);
+            //Product prod3 = new Product("3) Reduced-Fat TBE Breakfast Sandwich", "Food:", "Reduced-fat thin sliced smoked turkey, maple bacon, and cage-free egg whites on a flakey croissant.", 7.50);
+            //Product prod4 = new Product("4) Parmesan Spinach Knots", "Food:", "Fresh spinach baked in flakey buttermilk dough, brushed with a mix of butter, Parmesan, garlic powder, oregano and parsley.", 7.95);
+            //Product prod5 = new Product("5) Spiced Apple Pie Cake", "Food:", "Baked with freshly sliced apples and American Buttercream, with hints of cinnamon, nutmeg, and lemon zest.", 5.80);
+            //Product prod6 = new Product("6) Chocolate Chip Loaf", "Food:", "Baked soft with buttermilk, hints of vanilla and chocolate chips.", 3.50);
+            //Product prod7 = new Product("7) Cafe au Lait", "Drink:", "Hot whole milk, ground cinnamon, vanilla bean extract, sugar, unsweetened honey, orange & lemon zest.", 4.50);
+            //Product prod8 = new Product("8) Cortado", "Drink:", "Equal amount of espresso and steamed milk, served in a metal cup.", 5.80);
+            //Product prod9 = new Product("9) Cafe Americano", "Drink:", "Espresso and hot water brewed from the same source.", 4.00);
+            //Product prod10 = new Product("10) Cold Brew", "Drink:", "Coffee grounds steeped in cold water for 24 hours, filtered, and served cold.", 4.00);
+            //Product prod11 = new Product("11) Green Smoothie", "Drink:", "Chopped kale blended with almond milk, frozen banana slices, cinnamon, and hemp, chia, and ground flax seeds.", 8.50);
+            //Product prod12 = new Product("12) Hot Chocolate", "Drink:", "Ground cinnamon, unsweetened cocoa beans, vanilla extract, reduced-fat whipped topping. ", 6.50);
 
 
         }
